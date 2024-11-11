c800c0ddafe7d096759bca1a867d8738ff4c8e07
MrAbigor111 <128088614+Abigor111@users.noreply.github.com>
2024-11-11 17:56:12 +0000

    Update javascript so the form changes when the option selection on the scheduling menu is different

--- logic.js
+++ logic.js
@@ -24,3 +24,80 @@ document.addEventListener("DOMContentLoaded", () => {
         console.error("Template with id 'arrow-icon-template' not found.");
     }
 });
+const scheduleInputFly = document.getElementById('schedule_input_fly');
+const scheduleInputFlyHotel = document.getElementById('schedule_input_flyhotel');
+const scheduleInputFlyVehicle = document.getElementById('schedule_input_flyvehicle');
+const scheduleInputHotel = document.getElementById('schedule_input_hotel');
+const scheduleInputVehicle = document.getElementById('schedule_input_vehicle');
+function checkFormChange() {
+    if (scheduleInputFly.checked) {
+        const schedule_flight_hotel = document.getElementById("schedule-flight+hotel");
+        schedule_flight_hotel.style.display = 'none';        
+        const scheduleflight_vehicle = document.getElementById("schedule-flight+vehicle");
+        scheduleflight_vehicle.style.display = 'none';        
+        const schedule_hotel = document.getElementById("schedule-hotel");
+        schedule_hotel.style.display = 'none';
+        const schedule_vehicle = document.getElementById("schedule-vehicle");
+        schedule_vehicle.style.display = 'none';
+
+        const schedule_flight = document.getElementById("schedule-flight");
+        schedule_flight.style.display = 'block';
+    }
+    else if (scheduleInputFlyHotel.checked) {
+        const schedule_flight = document.getElementById("schedule-flight");
+        schedule_flight.style.display = 'none';
+        const scheduleflight_vehicle = document.getElementById("schedule-flight+vehicle");
+        scheduleflight_vehicle.style.display = 'none';        
+        const schedule_hotel = document.getElementById("schedule-hotel");
+        schedule_hotel.style.display = 'none';
+        const schedule_vehicle = document.getElementById("schedule-vehicle");
+        schedule_vehicle.style.display = 'none';
+ 
+        const schedule_flight_hotel = document.getElementById("schedule-flight+hotel");
+        schedule_flight_hotel.style.display = 'block';        
+    }
+    else if (scheduleInputFlyVehicle.checked) {
+        const schedule_flight = document.getElementById("schedule-flight");
+        schedule_flight.style.display = 'none';
+        const schedule_flight_hotel = document.getElementById("schedule-flight+hotel");
+        schedule_flight_hotel.style.display = 'none';        
+        const schedule_hotel = document.getElementById("schedule-hotel");
+        schedule_hotel.style.display = 'none';
+        const schedule_vehicle = document.getElementById("schedule-vehicle");
+        schedule_vehicle.style.display = 'none';
+
+        const scheduleflight_vehicle = document.getElementById("schedule-flight+vehicle");
+        scheduleflight_vehicle.style.display = 'block';        
+    }
+    else if (scheduleInputHotel.checked) {
+        const schedule_flight = document.getElementById("schedule-flight");
+        schedule_flight.style.display = 'none';
+        const schedule_flight_hotel = document.getElementById("schedule-flight+hotel");
+        schedule_flight_hotel.style.display = 'none';        
+        const scheduleflight_vehicle = document.getElementById("schedule-flight+vehicle");
+        scheduleflight_vehicle.style.display = 'none';        
+        const schedule_vehicle = document.getElementById("schedule-vehicle");
+        schedule_vehicle.style.display = 'none';
+
+        const schedule_hotel = document.getElementById("schedule-hotel");
+        schedule_hotel.style.display = 'block';
+    }
+    else if (scheduleInputVehicle.checked) {
+        const schedule_flight = document.getElementById("schedule-flight");
+        schedule_flight.style.display = 'none';
+        const schedule_flight_hotel = document.getElementById("schedule-flight+hotel");
+        schedule_flight_hotel.style.display = 'none';        
+        const scheduleflight_vehicle = document.getElementById("schedule-flight+vehicle");
+        scheduleflight_vehicle.style.display = 'none';        
+        const schedule_hotel = document.getElementById("schedule-hotel");
+        schedule_hotel.style.display = 'none';
+
+        const schedule_vehicle = document.getElementById("schedule-vehicle");
+        schedule_vehicle.style.display = 'block';
+    }
+};
+scheduleInputFly.addEventListener('change', checkFormChange);
+scheduleInputFlyHotel.addEventListener('change', checkFormChange);
+scheduleInputFlyVehicle.addEventListener('change', checkFormChange);
+scheduleInputHotel.addEventListener('change', checkFormChange);
+scheduleInputVehicle.addEventListener('change', checkFormChange);
